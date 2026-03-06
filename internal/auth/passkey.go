0503e4fb24debc43810ef994e5b31b6d8f68b243
qasim <alkuwaitiqasim@gmail.com>
2026-03-06 17:02:50 +0300

    build options

--- internal/auth/passkey.go
+++ internal/auth/passkey.go
@@ -4,34 +4,45 @@ import (
 	"context"
 	"time"
 
+	"github.com/alkuwaiti/auth/internal/auth/domain"
 	"github.com/alkuwaiti/auth/pkg/contextkeys"
 )
 
+type RP struct {
+	Name string
+	ID   string
+}
+
+type ExcludeCredential struct {
+	Type string
+	ID   string
+}
+
+type User struct {
+	ID          string
+	Name        string
+	DisplayName string
+}
+
+type PubKeyCredParam struct {
+	Type string
+	Alg  int
+}
+
+type AuthenticatorSelection struct {
+	ResidentKey      string
+	UserVerification string
+}
+
 type Options struct {
-	Challenge string
-	RP        struct {
-		Name string
-		ID   string
-	}
-	User struct {
-		ID          string
-		Name        string
-		DisplayName string
-	}
-	PubKeyCredParams []struct {
-		Type string
-		Alg  int
-	}
+	Challenge              string
+	RP                     RP
+	User                   User
+	PubKeyCredParams       []PubKeyCredParam
 	Timeout                int
 	Attestation            string
-	AuthenticatorSelection struct {
-		ResidentKey      string
-		UserVerification string
-	}
-	ExcludeCredentials []struct {
-		Type string
-		ID   string
-	}
+	AuthenticatorSelection AuthenticatorSelection
+	ExcludeCredentials     []ExcludeCredential
 }
 
 func (s *Service) StartPasskeyGeneration(ctx context.Context) (Options, error) {
@@ -55,8 +66,45 @@ func (s *Service) StartPasskeyGeneration(ctx context.Context) (Options, error) {
 		return Options{}, err
 	}
 
+	// TODO: change to store hashed.
 	if err = s.Repo.CreateWebAuthnChallenge(ctx, raw, userID, time.Now().Add(5*time.Minute)); err != nil {
 		return Options{}, err
 	}
 
+	return buildOptions(user, raw, creds), nil
+}
+
+func buildOptions(user domain.User, challenge string, creds [][]byte) Options {
+	exclude := make([]ExcludeCredential, len(creds))
+
+	for i, c := range creds {
+		exclude[i] = ExcludeCredential{
+			ID:   string(c),
+			Type: "public-key",
+		}
+	}
+
+	return Options{
+		Challenge: challenge,
+		RP: RP{
+			Name: "auth-service",
+			ID:   "localhost",
+		},
+		User: User{
+			ID:          user.ID.String(),
+			Name:        user.Email,
+			DisplayName: user.Email,
+		},
+		PubKeyCredParams: []PubKeyCredParam{
+			{Type: "public-key", Alg: -7},
+			{Type: "public-key", Alg: -257},
+		},
+		Timeout:     60000,
+		Attestation: "none",
+		AuthenticatorSelection: AuthenticatorSelection{
+			ResidentKey:      "preferred",
+			UserVerification: "preferred",
+		},
+		ExcludeCredentials: exclude,
+	}
 }
