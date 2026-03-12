8143b4fdca80ffcae459b98ae8bb2bdba2e91b9a
qasim <alkuwaitiqasim@gmail.com>
2026-03-12 17:03:25 +0300

    remove outbox fields

--- internal/auth/domain/outbox.go
+++ internal/auth/domain/outbox.go
@@ -1,19 +1,10 @@
 package domain
 
-import (
-	"time"
-
-	"github.com/google/uuid"
-)
-
 type OutboxEvent struct {
-	ID            uuid.UUID
 	AggregateType AggregateType
 	AggregateID   string
 	EventType     string
 	Payload       []byte
-	CreatedAt     time.Time
-	PublishedAt   *time.Time
 }
 
 type AggregateType string
